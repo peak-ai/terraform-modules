e6b714be24478bae98db810872a8605c2cc1bc95
Ricki Hastings <ricki.hastings@peak.ai>
2020-06-29 16:20:49 +0100

    Refactoring out the shell module, should be able to pass in these values from CI

--- docker_builder/main.tf
+++ docker_builder/main.tf
@@ -5,28 +5,17 @@ terraform {
   }
 }
 
-module "git_branch" {
-  source  = "matti/resource/shell"
-  command = "git rev-parse --abbrev-ref HEAD"
-}
-
-module "git_sha" {
-  source  = "matti/resource/shell"
-  command = "git rev-parse HEAD"
-}
-
 locals {
   args       = join(" ", var.args)
-  git_branch = lower(replace(module.git_branch.stdout, "/", "-"))
-  git_sha    = module.git_sha.stdout
+  git_branch = lower(replace(var.git_branch, "/", "-"))
 
   tag        = "${var.repository_url}:${local.git_branch}"
-  extra_tags = local.git_branch == "master" ? ["${var.repository_url}:${local.git_sha}", "${var.repository_url}:latest"] : ["${var.repository_url}:${local.git_sha}"]
+  extra_tags = local.git_branch == "master" ? ["${var.repository_url}:${var.git_sha}", "${var.repository_url}:latest"] : ["${var.repository_url}:${var.git_sha}"]
 }
 
 resource "null_resource" "build_image" {
   triggers = {
-    image_tag = module.git_sha.stdout
+    image_tag = var.git_sha
   }
 
   provisioner "local-exec" {
@@ -42,7 +31,7 @@ resource "null_resource" "build_image" {
         --cache-from ${local.tag} \
         --tag ${local.tag} \
         --build-arg CI="true" \
-        ${local.args} ${var.dockerfile};
+        ${local.args} ${var.path};
       docker push ${local.tag};
 
       %{for tag in local.extra_tags}
